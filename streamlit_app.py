a6a35a187497e3d42f1206b55eba6bf210140464
audy237 <cyrillengodobo@gmail.com>
2025-01-31 14:16:42 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,4 @@
 import streamlit as st
-st.title("welcome to NGODOBO Cyrille Dasboard for IRIS Zork January 2024")
+st.title("welcome to NGODOBO Cyrille Dasboard for IRIS Work January 2025")
 st.subheader("creating dasboard...")
 st.write("Hello Word")

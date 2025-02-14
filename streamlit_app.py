52337cde691ee5ed83be65a44654dd46b522832f
audy237 <cyrillengodobo@gmail.com>
2025-02-14 16:04:09 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,5 +4,5 @@ st.subheader("creating dasboard...")
 st.write("Hello Word")
 st.sidebar.slider('Longueur sépal', 0.0, 20.0)
 st.sidebar.slider('Largeur sépal', 0.0, 20.0)
-st.sidebar.slider('Longueur pétal', 0.0, 20.0)
+st.sidebar.slider('Longueur pétal', 0.0, 20.0,color) create-color#blue
 st.sidebar.slider('Largeur pétal', 0.0, 20.0)

31044e35ae004095379edfa818f75b9e41d1698d
audy237 <cyrillengodobo@gmail.com>
2025-02-14 13:04:19 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit as st
 st.title("welcome to NGODOBO Cyrille Dasboard for IRIS Work January 2025")
 st.subheader("creating dasboard...")
 st.write("Hello Word")
-st.sidebar.slider('valeur', , 10)
+st.sidebar.slider('valeur', 1, 20)
 1234
 st.text('Texte avec st.text')
 st.markdown('Texte avec st.markdown')
